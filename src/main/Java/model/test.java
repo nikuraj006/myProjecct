080b818c51cccd8ad44fe844566e13cfc65e99bb
Sushil <nikuraj006@gmail.com>
2018-03-03 21:29:46 +0530

    test

--- src/main/Java/model/test.java
+++ src/main/Java/model/test.java
@@ -5,11 +5,18 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
 
 @WebServlet("/test")
 public class test extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response){
-        /*
-        login here
-    */}
+        try {
+            PrintWriter out =  response.getWriter();
+            out.print(request.getParameter("name"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
